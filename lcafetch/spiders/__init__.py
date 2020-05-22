a7790a8a9f1c869adb56ee145dfa6e4601254738
Shantanu Joshi <weemadarthur@yggdrasil.in>
2020-05-23 03:33:29 +0530

    Resume crawl from previous run

--- lcafetch/spiders/__init__.py
+++ lcafetch/spiders/__init__.py
@@ -1,4 +1,5 @@
 import scrapy
+from sqlalchemy import create_engine, Table, MetaData, select
 
 class ComicSpider(scrapy.Spider):
 
@@ -10,4 +11,22 @@ class ComicSpider(scrapy.Spider):
 
     def start_requests(self):
 
-        return [scrapy.Request(self.start_url, callback = self.parse)]
+        engine = create_engine(self.crawler.settings.get('DATABASE_URL'))
+        metadata = MetaData(bind=engine)
+        comics = Table('comics', metadata, autoload=True)
+        archive = Table('archive', metadata, autoload=True)
+
+        with engine.connect() as connection:
+            result = connection.execute(select([archive.c.tag, archive.c.url])
+                                        .select_from(archive.join(comics))
+                                        .where(comics.c.nickname == self.name)
+                                        .order_by(archive.c.parsed_at.desc())
+                                        .limit(1))
+            row = result.first()
+            if row is not None:
+                self.prevtag = row['tag']
+                self.logger.info(f'Resuming crawl from {row["url"]}')
+                return [scrapy.Request(row['url'], callback = self.parse_next)]
+            else:
+                self.logger.info(f'Starting crawl from {self.start_url}')
+                return [scrapy.Request(self.start_url, callback = self.parse)]
