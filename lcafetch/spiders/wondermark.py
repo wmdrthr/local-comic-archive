e024f1cdbb07579c3feec5654287a03f1b4e484c
Shantanu Joshi <weemadarthur@yggdrasil.in>
2020-06-06 09:46:01 +0530

    Add comic spider: wondermark

---
+++ lcafetch/spiders/wondermark.py
@@ -0,0 +1,42 @@
+import re
+from datetime import datetime
+
+import scrapy
+
+import lcafetch.spiders
+
+class Wondermark(lcafetch.spiders.ComicSpider):
+
+    name = 'wondermark'
+    start_url = 'http://wondermark.com/001/'
+
+    def parse_next(self, response):
+
+        next_link = response.xpath('//a[@rel="next"]/@href').get()
+        if next_link is not None:
+            return scrapy.Request(next_link, callback = self.parse)
+        else:
+            return None
+
+    def parse(self, response):
+
+        date_text = response.xpath('//div[@id="comic-notes"]//em/text()').get()
+        comic_date = datetime.strptime(re.sub(r'(\d)(st|nd|rd|th)', r'\1', date_text),
+                                       '%B %d, %Y')
+        tag = int(comic_date.strftime('%Y%m%d'))
+
+        img = response.xpath('//div[@id="comic"]/img')
+        images = [img.attrib['src']]
+        captions = [img.attrib['alt']]
+
+        title_text = response.xpath('//head/title/text()').get()
+        title_text = title_text.split('»')[-1]
+        title = title_text.strip()
+
+        yield {'tag': tag,
+               'url': response.url,
+               'title': title,
+               'captions': captions,
+               'file_urls': images}
+
+        yield self.parse_next(response)
